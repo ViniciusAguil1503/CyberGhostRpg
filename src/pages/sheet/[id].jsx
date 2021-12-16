ca1aa1b1fbb826be8b21af4fde155e7dd7c12212
FELKIKS <92609299+FELKIKS@users.noreply.github.com>
2021-12-15 23:18:26 -0300

    Add files via upload

--- src/pages/sheet/[id].jsx
+++ src/pages/sheet/[id].jsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
-import IconButton from '@mui/material/IconButton';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
