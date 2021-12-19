fc9b6512a2f4e4c9e84e098955d8a72a04fa6842
FELKIKS <92609299+FELKIKS@users.noreply.github.com>
2021-12-19 04:27:51 -0300

    Add files via upload

--- src/src/theme/index.js
+++ src/src/theme/index.js
@@ -1,21 +1,20 @@
 import { createTheme } from '@mui/material/styles'
 
-const theme = createTheme
-const img = require(".../.../assets/fundo.jpg");
-const styling = {
-    backgroundimage: `url('${img}')`,
-    width:"100%",
-    height:"100%",
-    mode: 'dark',
-    primary:{
-        main: '#639EC2',
-        600: '#201E1E',
-        900: '#181717',
-    },
-    secondary:{
-        main: '#8c8c8c',
-      }
+const theme = createTheme({
+    palette: {
+        background: {
+            default: '#2b2b2b'
+        },
+        mode: 'dark',
+        primary: {
+            main: '#639EC2',
+            600: '#201E1E',
+            900: '#181717',
+        },
+        secondary: {
+            main: '#8c8c8c',
+        }
     }
+});
 
 export default theme
-
